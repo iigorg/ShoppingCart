456c92886d2fb53c3655851c5810c12a29c1874a
unknown <igordiiuk@gmail.com>
2019-01-28 16:49:22 +0200

    done

---
+++ js/app.js
@@ -0,0 +1,89 @@
+//show cart
+
+(function(){
+	const cartInfo = document.getElementById('cart-info');
+	const cart = document.getElementById('cart');
+
+	cartInfo.addEventListener('click', function(){
+	cart.classList.toggle('show-cart');
+
+	});
+})();
+
+//add items to the cart
+
+(function(){
+ const cartBtn = document.querySelectorAll('.store-item-icon');
+ cartBtn.forEach(function(btn){
+ 	btn.addEventListener('click', function(event){
+ 		// console.log(event.target);
+ 	if(event.target.parentElement.classList.contains('store-item-icon')){
+        let fullPath = event.target.parentElement.previousElementSibling.src;
+        //Метод IndexOf () возвращает позицию первого вхождения заданного значения в строке. //Найти первое вхождение буквы «е» в строке: var n = str.indexOf("e");
+        //Найти первое вхождение буквы «е» в строке, начиная поиск в позиции 5: var n = str.indexOf("e", 5);
+        let pos = fullPath.indexOf('img') + 3;
+
+        //Метод slice() извлекает текст из одной строки и возвращает новую строку. Изменения текста в одной строке не влияют на другую строку.
+        //Метод slice() извлекает все символы до индекса endSlice, не включая сам этот индекс. Вызов str.slice(1, 4) извлечёт символы со второго по четвёртый
+        let partPath = fullPath.slice(pos);
+
+        const item = {};
+        item.img = `img-cart${partPath}`;
+
+        let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+		item.name = name;
+         //Метод trim() удаляет пробельные символы с начала и конца строки. var orig = '   foo  '; console.log(orig.trim()); // 'foo'
+        let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+        let finalPrice = price.slice(1).trim();
+        item.price = finalPrice;
+        
+
+
+        const cartItem = document.createElement('div');
+       
+        cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+        cartItem.innerHTML = `
+        	<img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+            <div class="item-text">
+              <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+              <span>$</span>
+              <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+            </div>
+            <a href="#" id='cart-item-remove' class="cart-item-remove">
+              <i class="fas fa-trash"></i>
+            </a>
+           `;
+
+        //select cart
+        const cart = document.getElementById('cart');
+        const total = document.querySelector('.cart-total-container');
+          cart.insertBefore(cartItem, total);    //insert cartItem before total
+          alert('item added to the cart');
+          showTotals();
+        }
+ 	});
+ });
+
+//show totals
+function showTotals(){
+	const total = [];
+	const items = document.querySelectorAll('.cart-item-price');
+
+	items.forEach(function(item){
+		//Функция parseFloat() принимает строку в качестве аргумента и возвращает десятичное число (число с плавающей точкой)
+		total.push(parseFloat(item.textContent));
+	});
+
+	const totalMoney = total.reduce(function(total, item){
+		total += item;
+		return total;
+	},0);
+    // Метод toFixed() форматирует число, используя запись с фиксированной запятой. Количество цифр после десятичной запятой;
+	const finalMoney = totalMoney.toFixed(2);
+
+	document.getElementById('cart-total').textContent = finalMoney;
+	document.querySelector('.item-total').textContent = finalMoney;
+	document.getElementById('item-count').textContent = total.length;
+};
+
+})();
